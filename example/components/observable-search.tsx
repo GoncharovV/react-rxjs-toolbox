5a6ea7321b294481c130ae827f6d8a5a0e1ede33
Vadim Goncharov <Vadim2003Rus@yandex.ru>
2026-03-17 00:11:41 +0300

    feat: observable state example

---
+++ example/components/observable-search.tsx
@@ -0,0 +1,51 @@
+import { FC, useState } from 'react';
+import { BehaviorSubject } from 'rxjs';
+import { HStack, VStack } from '@goncharovv/layout';
+
+// TODO: import from dist
+import { useObservable, useObservableState } from '../../src';
+
+
+export const ObservableSearch: FC = () => {
+  const [search$] = useState(() => new BehaviorSubject<string>(''));
+
+  return (
+    <HStack spacing="medium-m" align="center">
+      <SearchInput search$={search$} />
+
+      <SearchPreview search$={search$} />
+    </HStack>
+  );
+};
+
+const SearchInput: FC<{ search$: BehaviorSubject<string>; }> = ({ search$ }) => {
+  const [search, setSearch] = useObservableState(search$);
+
+  return (
+    <VStack spacing="small-s" centered>
+      <p>Search</p>
+
+      <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
+    </VStack>
+  );
+};
+
+
+const SearchPreview: FC<{ search$: BehaviorSubject<string>; }> = ({ search$ }) => {
+  const search = useObservable(search$, search$.value);
+
+  return (
+    <VStack
+      spacing="small-l"
+      centered
+      style={{ border: '1px dashed coral' }}
+      p="small-s"
+      width={300}
+      height={300}
+    >
+      <p>Search Preview</p>
+
+      <h3>{search}</h3>
+    </VStack>
+  );
+};
--- example/main.tsx
+++ example/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { VStack } from '@goncharovv/layout';
 
+import { ObservableSearch } from './components/observable-search';
 import { ObservableTicker } from './components/observable-ticker';
 
 import '@goncharovv/layout/dist/styles/index.css';
@@ -11,6 +13,10 @@ const root = createRoot(document.getElementById('root')!);
 
 root.render(
   <StrictMode>
-    <ObservableTicker />
+    <VStack spacing="medium-m" align="center">
+      <ObservableTicker />
+
+      <ObservableSearch />
+    </VStack>
   </StrictMode>,
 );
