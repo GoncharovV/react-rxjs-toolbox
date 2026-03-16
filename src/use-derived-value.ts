a7f076e79ce5a3970e1aa54f589a6fc1b641bb33
Vadim Goncharov <Vadim2003Rus@yandex.ru>
2026-03-17 02:25:25 +0300

    feat: comparator for useDistinctDerivedValue

--- src/use-derived-value.ts
+++ src/use-derived-value.ts
@@ -27,14 +27,15 @@ export function useDerivedValue<TValue, TDerived>(
 export function useDistinctDerivedValue<TValue, TDerived>(
   observable: Observable<TValue>,
   producer: (value: TValue) => TDerived,
+  comparator?: (a: TDerived, b: TDerived) => boolean,
 ) {
-  const producerRef = useRef(producer);
-  producerRef.current = producer;
+  const ref = useRef({ producer, comparator });
+  ref.current = { producer, comparator };
 
   const derivedObservable = useMemo(() => {
     return observable.pipe(
-      map((value) => producerRef.current(value)),
-      distinctUntilChanged(),
+      map((value) => ref.current.producer(value)),
+      distinctUntilChanged(ref.current.comparator),
     );
   }, [observable]);
 
